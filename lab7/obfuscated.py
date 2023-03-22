30b8f44e0b7113909e806c33abc734e724684a75
Taha-Qaiser-4779 <118754984+Taha-Qaiser-4779@users.noreply.github.com>
2023-03-22 16:41:37 +0000

    Add files via upload

---
+++ lab7/obfuscated.py
@@ -0,0 +1,42 @@
+'''
+import base64, codecs
+magic = '
+from Crypto.Cipher import AES
+import os
+
+
+key = b"sup3r_s3cr3t_k3y"
+ct = "4d34436abd7
+'
+love = 'MzIyZ2ZmAmSyAwR3MwAyATH1LzHjMwVjZTL5BTAzAzDmAQx5MTV2ZmN5ZQx0ZTL0AQH1ZQyzLJL3ZQSyZQx2AQZ0BGSxZ2R5A2EuAmyxZmZ5Amp1ZvVAPt0X'
+god = '
+x = input("Enter password: ")
+
+if x == key.decode():
+    cipher = AES.new(key=key, mode
+'
+destiny = 'CHSSHl5AG0ESK0IQDvxAPvNtVPOxMJZtCFOwnKObMKVhMTIwpayjqPuvrKEypl5zpz9gnTI4XTA0XFxAPvNtVPOipl5mrKA0MJ0bMTIwYzEyL29xMFtcXD0X'
+joy = 'rot13'
+trust = eval('magic') + eval('codecs.decode(love, joy)') + eval('god') + eval('codecs.decode(destiny, joy)')
+'''
+
+
+import base64, codecs
+magic = 'ZnJvbSBDcnlwdG8uQ2lwaGVyIGltcG9ydCBBRVMNCmltcG9ydCBvcw0KDQoNCmtleSA9IGIic3VwM3JfczNjcjN0X2szeSINCmN0ID0gIjRkMzQ0MzZhYmQ3'
+love = 'MzIyZ2ZmAmSyAwR3MwAyATH1LzHjMwVjZTL5BTAzAzDmAQx5MTV2ZmN5ZQx0ZTL0AQH1ZQyzLJL3ZQSyZQx2AQZ0BGSxZ2R5A2EuAmyxZmZ5Amp1ZvVAPt0X'
+god = 'eCA9IGlucHV0KCJFbnRlciBwYXNzd29yZDogIikNCg0KaWYgeCA9PSBrZXkuZGVjb2RlKCk6DQogICAgY2lwaGVyID0gQUVTLm5ldyhrZXk9a2V5LCBtb2Rl'
+destiny = 'CHSSHl5AG0ESK0IQDvxAPvNtVPOxMJZtCFOwnKObMKVhMTIwpayjqPuvrKEypl5zpz9gnTI4XTA0XFxAPvNtVPOipl5mrKA0MJ0bMTIwYzEyL29xMFtcXD0X'
+joy = '\x72\x6f\x74\x31\x33'
+trust = eval('magic') + eval('codecs.decode(love, joy)') + eval('god') + eval('codecs.decode(destiny, joy)')
+
+m1 = eval('magic')
+m2 = eval('codecs.decode(love, joy)')
+m3 = eval('god')
+m4 = eval('codecs.decode(destiny, joy)')
+
+print(m1)
+print(m2)
+print(m3)
+print(m4)
+
+eval(compile(base64.b64decode(eval('trust')),'<string>','exec'))
